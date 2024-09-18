a3d91982fa58c3ed824f7fc2c349437ccacad68d
Claudio01420 <A00835868@tec.mx>
2024-09-18 13:26:16 -0600

    Cambios: Remover contraseña

--- my-app/public/JavaScrip/login.js
+++ my-app/public/JavaScrip/login.js
@@ -5,12 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
 
         const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
 
         // Aquí deberías agregar la lógica para autenticar al usuario
         // Para el propósito de este ejemplo, usaremos un simple almacenamiento local para la autenticación
 
-        if (username === '1' && password === '1') {
+        if (username === '1') {
             localStorage.setItem('loggedIn', 'true');
             window.location.href = 'Página.html'; // Redirige al usuario a la página principal
         } else {
