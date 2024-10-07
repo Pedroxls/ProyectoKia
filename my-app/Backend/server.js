6e520134a4de241d2754d1e81107b5faec56791f
Claudio01420 <A00835868@tec.mx>
2024-10-07 04:53:07 -0600

    Corss

--- my-app/Backend/server.js
+++ my-app/Backend/server.js
@@ -4,6 +4,8 @@ const sql = require('mssql');
 const bodyParser = require('body-parser');
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
